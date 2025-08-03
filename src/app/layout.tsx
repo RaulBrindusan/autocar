ae3e88744bde3a97ba5315868566d83bb93cc6bc
RaulBrindusan <raulbrindusan@gmail.com>
2025-08-03 17:26:27 +0300

    oiio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="light">
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white dark:bg-gray-900 text-gray-900 dark:text-white transition-colors`}
       >
